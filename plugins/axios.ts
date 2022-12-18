3eec899d94e075e12abea1b08c11686bc3cd436f
marco94gug <marco.guglielmino94@gmail.com>
2022-12-18 13:36:44 +0100

    feat: added env property

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -5,8 +5,7 @@ const plugin: Plugin = function ({ app: { $axios } }) {
   $axios.onRequest((config) => {
     config.headers.common.Accept = "*/*";
     config.headers.common["Content-Type"] = "application/json";
-    config.headers.common["X-RapidAPI-Key"] =
-      "5d99ed1e87msh336ffda5f50f370p177d16jsnc01cfe766594";
+    config.headers.common["X-RapidAPI-Key"] = process.env.API_KEY;
     config.headers.common["X-RapidAPI-Host"] = "the-cocktail-db.p.rapidapi.com";
   });
 };
